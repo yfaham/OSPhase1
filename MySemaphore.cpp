fd2f2106e61fe813f8792b201698df49122ad242
Joseph Venanzio <vivacious640@gmail.com>
2019-02-23 00:40:36 -0500

    s

--- MySemaphore.cpp
+++ MySemaphore.cpp
@@ -1,37 +1,65 @@
 #include "MyQueue.h"
+#include "MySemaphore.h"
 #include <cstdio>
-#include <ncurse.h>
-#include <pthread.h>
+#include <ncurses.h>
 #include <unistd.h>
 #include <cstdarg>
-#include <thread>
 #include <mutex>
+#include <thread>
+#include <pthread.h>
 
 using namespace std;
 
+MyQueue<int> q;
+
 mutex mtx;  // mutex for critical section
 
-MySemaphore::down(int Tid) {
+void MySemaphore::down(int Tid) {
     
     if(sema_value == 1)
+    {
+        mtx.lock();
         sema_value--;
+    }
     else
     {
+        q.enqueue(Tid);
         
-        q.push(Tid);
-        //Myschedule->(setState, Tid, Block);
-        //SchedPtr->process_table.at(index)->state
-        
-        
+        for (int i = 0; i < Scheduler->process_table.getLength(); i++) {
+            Scheduler->process_table.at(i)->state = Scheduler->BLOCKED;
+        }
     }
 }
 
-MySemaphore::up() {
-    sema_value++;
-    q.pop();
+void MySemaphore::up() {
     
+    if(!q.isEmpty())
+    {
+        q.dequeue();
+        mtx.unlock();
+    }
+    else
+    {
+        sema_value++;
+        mtx.unlock();
+    }
 }
 
-MySemaphore::dump() {
+void MySemaphore::dump() {
     
+    cout << "Resource: " << resource_name;
+    //for(int i =0 ; i < q.getLength(); i++)
+      //  cout << *(resource_name);
+    Scheduler -> yield();
+    
+    cout << "Sema_value: " << sema_value << endl;
+    Scheduler -> yield();
+    
+    cout << "Sema_queue: ";
+    for(int i = 0 ; i < q.getLength(); i++)
+    {
+        cout << sema_queue.front() << " ";
+        sema_queue.dequeue();
+    }
+    Scheduler -> yield();
 }
--- MySemaphore.h
+++ MySemaphore.h
@@ -1,15 +1,22 @@
+#ifndef MYSEMAPHORE_H
+#define MYSEMAPHORE_H
+
 #include "MyQueue.h"
+#include "MyScheduler.h"
 
 using namespace std;
 
 class MySemaphore {
  public:
-  down(int);
-  up();
-  dump();
+  void down(int);
+  void up();
+  void dump();
 
  private:
   char *resource_name;
   int sema_value;
-  MyQueue sema_queue;
-}
+  MyQueue<int> sema_queue;
+  MyScheduler *Scheduler;
+};
+
+#endif
